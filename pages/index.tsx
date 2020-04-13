976e5f85901ae30a2df9e4702784294d66f32954
Thiago Ribeiro <thiago.ribeiro@0450-mbr.localdomain>
2020-04-13 19:33:41 +0200

    Logos and mobile/desktop detect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,33 @@
 import Head from 'next/head'
+import { useState } from 'react'
+import cx from 'classnames'
+import { isMobile } from 'react-device-detect'
 
-const renderTimeline = ({ userName }) => {
+type Timeline = {
+  userName: string
+  logo: string
+  flag?: string
+}
+
+const timelines: Timeline[] = [
+  { userName: 'HerthaBSC', flag: '🇩🇪', logo: '/assets/hertha-logo-de.jpg' },
+  { userName: 'HerthaBSC_EN', flag: '🇬🇧', logo: '/assets/hertha-logo-en.jpg' },
+  { userName: 'HerthaBSC_ES', flag: '🇪🇦', logo: '/assets/hertha-logo-en.jpg' },
+  {
+    userName: 'HerthaBubis',
+    logo: '/assets/hertha-logo-bubis.jpg',
+    // flag: '🇬🇧',
+  },
+  {
+    userName: 'kicker_BSC',
+    logo: '/assets/hertha-logo-kicker.jpg',
+    // flag: '🇬🇧',
+  },
+]
+
+const renderTimeline: React.FC<Timeline> = ({ userName }) => {
   return (
-    <div className="flex-1 overflow-y-auto p-1">
+    <div className="flex-1 overflow-y-auto p-1" key={userName}>
       <a
         data-theme="dark"
         className="twitter-timeline"
@@ -19,48 +44,143 @@ const renderTimeline = ({ userName }) => {
   )
 }
 
-const Index = () => (
-  <div className="wrapper">
-    <Head>
-      <title>Hertha BSC News</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+type ProfileLogosProps = {
+  activeList: Object
+  onClick: Function
+}
 
-    <main className="flex flex-col absolute w-full h-full overflow-hidden">
-      <div className="navBar flex px-4 items-center bg-white">
-        <h1 className="text-black text-lg pl-1 font-bold">
-          🔵⚪️ Hertha BSC News 🔵⚪️
-        </h1>
-        <div className="flex-1 flex flex-col items-end pr-1">
-          <button onClick={() => window.location.reload()}>Refresh 🔄</button>
+const ProfileLogos: React.FC<ProfileLogosProps> = ({ activeList, onClick }) => {
+  return (
+    <div className="flex">
+      {timelines.map((timeline, index) => (
+        <div
+          className={cx('pl-1 pr-2 relative', {
+            inactive: !activeList[timeline.userName],
+          })}
+          key={index}
+          onClick={() => onClick(timeline.userName)}
+        >
+          <img
+            src={timeline.logo}
+            className="timeline-logo sm:h-16 border-white border-2 cursor-pointer"
+            // h-12
+          />
+          {timeline.flag && (
+            <span className="timeline-logo-flag absolute text-2xl sm:text-3xl pl-8 sm:pl-12">
+              {timeline.flag}
+            </span>
+          )}
         </div>
-      </div>
+      ))}
+      <style jsx>{`
+        .timeline-logo {
+          border-radius: 50%;
+        }
 
-      <div className="flex flex-row h-full p-4">
-        {renderTimeline({ userName: 'HerthaBSC' })}
-        {renderTimeline({ userName: 'HerthaBSC_EN' })}
-        {renderTimeline({ userName: 'HerthaBSC_ES' })}
-        {renderTimeline({ userName: 'HerthaBubis' })}
-        {renderTimeline({ userName: 'kicker_BSC' })}
-      </div>
-    </main>
+        .timeline-logo-flag {
+          bottom: 0px;
+        }
 
-    <style jsx>{`
-      main {
-        background-color: #005ca9;
-        background-image: linear-gradient(
-          90deg,
-          transparent 50%,
-          rgba(255, 255, 255, 0.2) 50%
-        );
-        background-size: 100px 100px;
-      }
+        .inactive {
+          filter: grayscale(100%);
+          opacity: 0.8;
+        }
+      `}</style>
+    </div>
+  )
+}
 
-      .navBar {
-        min-height: 50px;
-      }
-    `}</style>
-  </div>
-)
+const initialState: Object = { HerthaBSC: true }
+// const initialState: Object = isMobile
+//   ? {
+//       HerthaBSC: true,
+//     }
+//   : timelines.reduce((acc, timeline) => {
+//       acc[timeline.userName] = true
+//       return acc
+//     }, {})
+
+const Index: React.FC = () => {
+  const [activeList, setActive] = useState(initialState)
+
+  console.log(isMobile)
+  console.log(activeList)
+
+  const toggleItem = (item) => {
+    console.log('toggleItem', item)
+    if (isMobile) {
+      const disableAll = timelines.reduce((acc, timeline) => {
+        acc[timeline.userName] = false
+        return acc
+      }, {})
+
+      setActive({
+        ...disableAll,
+        [item]: !activeList[item],
+      })
+    } else {
+      setActive({
+        ...activeList,
+        [item]: !activeList[item],
+      })
+    }
+
+    window['twttr'].widgets.load()
+  }
+
+  return (
+    <div className="wrapper">
+      <Head>
+        <title>Hertha BSC News</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col absolute w-full h-full overflow-hidden">
+        {/* navbar */}
+        <div className="navBar flex px-4 items-center bg-white">
+          <h1 className="text-black text-lg pl-1 font-bold">
+            Hertha BSC News 🔵⚪️
+          </h1>
+          <div className="flex-1 flex flex-col items-end pr-1 text-2xl">
+            <button onClick={() => window.location.reload()}>🔄</button>
+          </div>
+        </div>
+
+        {/* mobile icons */}
+        {/* lg:hidden */}
+        <div className="px-4 pt-4">
+          <ProfileLogos
+            activeList={activeList}
+            onClick={(userName) => toggleItem(userName)}
+          />
+        </div>
+
+        {/* timelines */}
+        <div className="flex flex-row h-full p-4">
+          {timelines.map(
+            (timeline) =>
+              activeList[timeline.userName] && renderTimeline(timeline)
+          )}
+        </div>
+      </main>
+
+      <style jsx>{`
+        main {
+          background-color: #005ca9;
+          background-image: linear-gradient(
+            90deg,
+            transparent 50%,
+            rgba(255, 255, 255, 0.2) 50%
+          );
+          background-size: 100px 100px;
+        }
+
+        .navBar {
+          min-height: 50px;
+        }
+      `}</style>
+    </div>
+  )
+}
 
 export default Index
